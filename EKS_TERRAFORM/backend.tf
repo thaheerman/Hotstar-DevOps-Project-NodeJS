957318dd7449008a43264d4c6deee62027bcf6f2
thaheerman <145525025+thaheerman@users.noreply.github.com>
2024-02-13 16:16:27 +0530

    backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "thaheer-1" # Replace with your actual S3 bucket name
+    bucket = "thaheer-2" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
